2d67be8c5d4013a688fcf002975c6084b08e7b65
alkasm <ar@reynoldsalexander.com>
2017-08-12 02:39:56 -0700

    Conforming to PEP, removing unused imports

--- example.py
+++ example.py
@@ -1,18 +1,15 @@
 import cspaceSliders
-import cspaceThreshImg
 import cv2
-import numpy as np
 
 img = cv2.imread('lane.jpg')
 
-mask, masked_img, cspace_label, lowerb, upperb = cspaceSliders.display(img, multi_window=False)
+mask, masked_img, cspace_label, lowerb, upperb = cspaceSliders.display(img)
 
 cv2.imshow("Mask", mask)
 cv2.waitKey(0)
 cv2.imshow("Applied Mask", masked_img)
 cv2.waitKey(0)
 
-print(
-    '\nColorspace:', cspace_label,
-    '\nLower bound:', lowerb,
-    '\nUpper bound:', upperb)
+print('\nColorspace:', cspace_label,
+      '\nLower bound:', lowerb,
+      '\nUpper bound:', upperb)
