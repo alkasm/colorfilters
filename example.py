07854405ba58e9ad84debc535ff840f697d68030
alkasm <ar@reynoldsalexander.com>
2017-09-12 06:03:35 -0700

    Object oriented overhaul

--- example.py
+++ example.py
@@ -1,15 +1,17 @@
-import cspaceSliders
+from cspaceSliders import FilterWindow
 import cv2
 
-img = cv2.imread('lane.jpg')
 
-mask, masked_img, cspace_label, lowerb, upperb = cspaceSliders.display(img)
+image = cv2.imread('lane.jpg')
+window = FilterWindow('Filter Window', image)
+window.show(verbose=True)
 
-cv2.imshow("Mask", mask)
-cv2.waitKey(0)
-cv2.imshow("Applied Mask", masked_img)
-cv2.waitKey(0)
+colorspace = window.colorspace
+lowerb, upperb = window.bounds
+mask = window.mask
+applied_mask = window.applied_mask
 
-print('\nColorspace:', cspace_label,
-      '\nLower bound:', lowerb,
-      '\nUpper bound:', upperb)
+print('Displaying the image with applied mask filtered in', colorspace,
+      '\nwith lower bound', lowerb, 'and upper bound', upperb)
+cv2.imshow('Applied Mask', applied_mask)
+cv2.waitKey()
