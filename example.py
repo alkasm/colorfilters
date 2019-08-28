38cb8583f9cedef4e8ebcb5725315e4b79f2ad93
alkasm <ar@reynoldsalexander.com>
2019-08-28 03:14:00 -0700

    Overhaul. Replaced single window with multiple colorspace windows. Added a main module entrypoint for running python3 -m ...

--- example.py
+++ example.py
@@ -1,17 +1,8 @@
-from cspaceSliders import FilterWindow
-import cv2
+from colorfilters import HSVFilter
+import cv2 as cv
 
+img = cv.imread("lane.jpg")
+window = HSVFilter(img)
+window.show()
 
-image = cv2.imread('lane.jpg')
-window = FilterWindow('Filter Window', image)
-window.show(verbose=True)
-
-colorspace = window.colorspace
-lowerb, upperb = window.bounds
-mask = window.mask
-applied_mask = window.applied_mask
-
-print('Displaying the image with applied mask filtered in', colorspace,
-      '\nwith lower bound', lowerb, 'and upper bound', upperb)
-cv2.imshow('Applied Mask', applied_mask)
-cv2.waitKey()
+print(f"Image filtered in HSV between {window.lowerb} and {window.upperb}.")
